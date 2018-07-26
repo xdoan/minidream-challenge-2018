7eed00c4a84581fec093add4389ebd8f662221d4
James Eddy <james.a.eddy@gmail.com>
2018-07-26 09:06:42 -0700

    add initial scoring code for module 6

---
+++ modules/module6/.eval/eval_fxn.R
@@ -0,0 +1,26 @@
+library(yaml)
+library(tidyverse)
+library(glue)
+library(rprojroot)
+
+
+root_dir <- find_root(is_rstudio_project, thisfile())
+data_dir <- file.path(root_dir, "data")
+
+score_submission <- function(submission_filename) {
+
+  answers <- yaml.load_file(submission_filename)
+
+  # goldstandard_df <- select(metabric_val_clin_df, metabric_id, T)
+  # submission_df <- as.tibble(answers$prediction) %>%
+  #   mutate(T = as.integer(T))
+  #
+  # check_df <- left_join(submission_df, goldstandard_df, by = "metabric_id")
+  # print(plot(check_df$T.x, check_df$T.y))
+  # s <- summary(lm(T.y ~ T.x, data = check_df))
+  # answers$r_squared <- sprintf("%0.4f", s$r.squared)
+  # answers$rmse <- sprintf("%0.3f", sqrt(mean(s$residuals^2)))
+  # answers$prediction <- NULL
+  
+  answers
+}
