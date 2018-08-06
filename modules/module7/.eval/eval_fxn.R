773bf32404c5f47b8d21d9b0340cb96f9f68bc00
James Eddy <james.a.eddy@gmail.com>
2018-08-06 19:06:55 +0000

    replace str_glue with glue in module 7 scoring

--- modules/module7/.eval/eval_fxn.R
+++ modules/module7/.eval/eval_fxn.R
@@ -12,7 +12,7 @@ score_submission <- function(submission_filename) {
   cms_match <- answers$cms_match
 
   if (high_cellline == "T-47D" & cms_match == "no") {
-    msg <- str_glue(
+    msg <- glue(
       "Your interpretation matches what I found — T-47D appears to have a ", 
       "greater abundance of clutches and motors, based on the expression of ",
       "37 cell adhesion and 68 myosin genes, respectively. Based on this ",
@@ -20,7 +20,7 @@ score_submission <- function(submission_filename) {
       "in the higher stiffness condition — but that's not the case."
     )
   } else if (high_cellline == "MDA-MB-231" & cms_match == "yes") {
-    msg <- str_glue(
+    msg <- glue(
       "I can see how, if you interpreted MDA-MB-231 as the 'high motors and ",
       "clutches' cell line, then the fact that this cell line to exhibits ",
       "greater motility in the higher stiffness condition would indeed match ",
@@ -29,7 +29,7 @@ score_submission <- function(submission_filename) {
       "respectively, that we examined here."
     )
   } else {
-    msg <- str_glue(
+    msg <- glue(
       "I'm not sure how you reached that particular conclusion. Check out ",
       "some of the other submissions for a couple interpretations that we'd ",
       "expect to see, given the data used."
